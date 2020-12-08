328d9fa1e6ea45056b0877b6d254134c49fb4578
Olivia Charles <ocharles92@gmail.com>
2020-12-07 21:43:54 -0500

    Created cell_test.rb, finished part 1

---
+++ test/cell_test.rb
@@ -0,0 +1,36 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ship'
+require './lib/cell'
+
+class CellTest < Minitest::Test
+
+  def test_it_exist
+    cell = Cell.new("B4")
+    assert_instance_of Cell, cell
+  end
+
+  def test_it_has_attributes
+    cell = Cell.new("B4")
+    assert_equal "B4", cell.coordinate
+  end
+
+  def test_ship_exists
+    cell = Cell.new("B4")
+    assert_nil nil,  cell.ship
+  end
+
+  def test_empty?
+    cell = Cell.new("B4")
+    assert_equal true, cell.empty?
+  end
+
+  def test_place_ship
+    cell = Cell.new("B4")
+    cruiser = Ship.new("Cruiser", 3)
+    cell.place_ship(cruiser)
+    assert_equal [cruiser], cell.ship
+
+    assert_equal false, cell.empty?
+  end
+end
