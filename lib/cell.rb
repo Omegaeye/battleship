3d8c85d64d8dde82783b318ade3bdc564db9d747
Olivia Charles <ocharles92@gmail.com>
2020-12-07 21:43:11 -0500

    Created cell.rb, finished part 1

---
+++ lib/cell.rb
@@ -0,0 +1,18 @@
+class Cell
+attr_reader :coordinate,
+            :ship
+
+  def initialize(coordinate)
+    @coordinate = coordinate
+    @ship       = nil
+
+  end
+
+  def empty?
+    @ship.nil?
+  end
+
+  def place_ship(cruiser)
+    @ship = [] << cruiser
+  end
+end
